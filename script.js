0f0adb20e3d1af9d50efacf9a2c20ba15b137b07
Victor Portugal <victorportugalnobrega@gmail.com>
2025-11-22 10:45:21 -0300

    Povoando a base de dados e adicionando a busca

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+let cardContainer = document.querySelector(".card-container")
+let campoBusca = document.querySelector("header input")
 let dados = []
 
 async function iniciarBusca(){
+    if(dados.length === 0){
+        try{
+            let resposta = await fetch("data.json")
+            dados = await resposta.json()
+        } catch(error){
+            console.error("Erro ao carregar os dados:", error)
+            return
+        }
+    }
+    const termoBusca = campoBusca.value.toLowerCase()
+    const dadosFiltrados = dados.filter(dado => dado.nome.toLowerCase().includes(termoBusca) || dado.genero.toLowerCase().includes(termoBusca) || dado.autor.toLowerCase().includes(termoBusca))
+    renderizarCards(dadosFiltrados)
+}
+
+async function carregarDados(){
+
     let resposta = await fetch("data.json")
     dados = await resposta.json()
-    console.log(dados)
+    renderizarCards(dados)
+}
+
+function renderizarCards(dados){
+    for(let dado of dados){
+        let article = document.createElement("article")
+        article.classList.add("card")
+        article.innerHTML = `
+            <h2>${dado.nome}</h2>
+            <p>${dado.sinopse}</p>
+            <p>${dado.genero}</p>
+            <p>${dado.ano_lancamento}</p>
+            <p>${dado.autor}</p>
+            <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `
+        cardContainer.appendChild(article)
+    }
 }
